b84ffafa747610b839a77d35443055ef4ce96ef5
MoonKing <79813883+VanLin444@users.noreply.github.com>
2022-04-26 00:32:51 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$apiKey = "23ebb64dedfd932a3fccffd0ddd80fe1";
+$cityId = "524901";
+$googleApiUrl = "http://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&lang=en&units=metric&APPID=" . $apiKey;
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
+
+curl_close($ch);
+$data = json_decode($response);
+$currentTime = time();
+?>
