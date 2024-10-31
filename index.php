e4ed723bf6dbfc2bcbadcdbc7a797c3947a1e837
Ivan <onel1n.van@yandex.ru>
2024-10-31 18:47:35 +0300

    add icons and centering

--- index.php
+++ index.php
@@ -1,34 +1,27 @@
+<?php include 'request.php';?>
 <!doctype html>
 <html>
 <head>
 <title>WeatherApp</title>
-
 <link rel='stylesheet' href='style.css' />
-
 </head>
-
 <body>
-    <?php include 'page.php';?>
-
     <div class="report-container">
         <h2><?php echo $data->name; ?> Weather Status</h2>
         <div class="time">
-            <div><?php echo date("l g:i a", $currentTime); ?></div>
+            <div><?php echo date("l G:i a", $currentTime); ?></div>
             <div><?php echo date("jS F, Y",$currentTime); ?></div>
-            <div><?php echo ucwords($data->weather[0]->description); ?></div>
         </div>
         <div class="weather-forecast">
-            <img
-                src="http://openweathermap.org/img/w/<?php echo $data->weather[0]->icon; ?>.png"
-                class="weather-icon" /> <?php echo $data->main->temp_max; ?>&deg;C<span
-                class="min-temperature"><?php echo $data->main->temp_min; ?>&deg;C</span>
+            <div><?php echo ucwords($data->weather[0]->description); ?></div>
+            <img src="http://openweathermap.org/img/w/<?php echo $data->weather[0]->icon; ?>.png" class="weather-icon"> 
+            <span class="max-temperature"><?php echo $data->main->temp_max; ?>&deg;C</span>
+            <span class="min-temperature"><?php echo $data->main->temp_min; ?>&deg;C</span>
         </div>
         <div class="time">
-            <div>Humidity: <?php echo $data->main->humidity; ?> %</div>
-            <div>Wind: <?php echo $data->wind->speed; ?> km/h</div>
+            <div><img src="https://icon666.com/r/_thumb/7zd/7zdkkbsr7qa5_64.png" width="45" height="45" class="weather-icon">Humidity: <?php echo $data->main->humidity; ?> %</div>
+            <div><img src="https://icon-library.com/images/windy-weather_16590.png" width="45" height="45" class="weather-icon">Wind: <?php echo $data->wind->speed; ?> km/h</div>
         </div>
     </div>
-
-
 </body>
 </html>
