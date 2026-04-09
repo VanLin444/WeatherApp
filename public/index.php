ea069d86be169e44e45f7cc0613555f54cd5d451
VanLin <onel1n.van@yandex.ru>
2026-04-09 19:17:50 +0300

    make front

--- public/index.php
+++ public/index.php
@@ -4,8 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WeatherApp</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    
+    <div class="wrapper">
+        <div class="current-weather">
+            <H1><span>+11°</span>Kazan</H1>
+            <div class="weather-icon"><img src="" alt="icon"></div>
+            <ul class="weather-stats">
+                <li><span></span>4,1 м/с, Ю</li>
+                <li><span></span>730</li>
+                <li><span></span>48%</li>
+            </ul>
+        </div>
+    </div>
 </body>
+
+<footer>
+    © 2026 VanLin444 | GitHub Projects Hub
+</footer>
 </html>
